2b0619ee1ba602ccef79656ae210510974930972
Keyla Martinez <martinezkey0306@gmail.com>
2022-10-10 09:03:29 -0500

    anova

--- Datos/Clase_S9D2.R
+++ Datos/Clase_S9D2.R
@@ -12,5 +12,48 @@ tapply(paraje$DAP, paraje$Paraje, median)
 tapply(paraje$DAP, paraje$Paraje, var)
 tapply(paraje$DAP, paraje$Paraje, length)
 
-boxplot(paraje$DAP ~ paraje$Paraje, col="purple")
+boxplot(paraje$DAP ~ paraje$Paraje, 
+        xlab = "Paraje", 
+        ylab = "DAP¨(cm)",
+        col="purple")
+
+
+# Semana 10 Dia 1 10/10/2021
+#Homogeneidad de varianza barlett.test
+
+bartlett.test(paraje$DAP, paraje$Paraje)
+
+#Ho = las varianzas son homogeneas 
+#H1 = las varianzas no son homogeneas 
+
+#Normalidad de la variable DAP 
 shapiro.test(paraje$DAP)
+hist(paraje$DAP)
+
+library(dplyr)
+
+Chinatu <- paraje %>%
+  filter(Paraje == "Chinatu")
+Trinidad <- paraje %>%
+  filter(Paraje == "Trinidad")
+Laguna <- paraje %>%
+  filter(Paraje == "Laguna")
+Tule <- paraje %>%
+  filter(Paraje == "Tule")
+
+shapiro.test(chinatu$DAP)
+shapiro.test(Trinidad$DAP)
+shapiro.test(Laguna$DAP)
+shapiro.test(Tule$DAP)
+
+par.aov <- aov(paraje$DAP ~ paraje$Paraje)
+summary(par.aov)
+
+#Como la prueba de ANOVA (AOV) me dice que hay diferencias significativas, entonces procedo con la prueba de Tuckey, 
+#la cual identificara donde estan las diferencias
+
+TukeyHSD(par.aov)
+
+plot(TukeyHSD(par.aov))
+
+
